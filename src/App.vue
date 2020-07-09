1bc70591aaef18fbcb45030f553a4a1464f76316
EngineerJill <62292583+EngineerJill@users.noreply.github.com>
2020-07-09 20:35:04 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="nav-bar">
+      <h3 class="logo">GWA Calculator</h3>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+   
+   }
+</script>
+
+<style>
+     * {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .nav-bar {
+        height: 50px;
+        width: 100%;
+        background-color: #07689F;
+        position: relative;
+    }
+    
+    .logo {
+        position: absolute;
+        top: 20%;
+        font-family: 'Times New Roman', Times, serif;
+        color: white;
+        font-size: 1.5rem;
+        left: 3%;
+    }
+
+</style>
