ecefd33db65b49f1c1b1b1915fd6d69550531326
EngineerJill <62292583+EngineerJill@users.noreply.github.com>
2020-07-11 21:06:39 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div class="nav-bar">
-      <h3 class="logo">GWA Calculator</h3>
+      <h3 class="logo">AVERAGES | <span class="lite">Compute your GWA here</span></h3>
     </div>
     <router-view />
   </div>
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style>
-     * {
+    * {
         margin: 0;
         padding: 0;
         font-family: Arial, Helvetica, sans-serif;
@@ -26,15 +26,22 @@ export default {
         width: 100%;
         background-color: #07689F;
         position: relative;
+        box-shadow: 0px 2px 3px gray;
     }
     
     .logo {
         position: absolute;
-        top: 20%;
-        font-family: 'Times New Roman', Times, serif;
+        top: 30%;
+        font-family: Arial, Helvetica, sans-serif;
         color: white;
-        font-size: 1.5rem;
-        left: 3%;
+        font-size: 1.0rem;
+        left: 6%;
+    }
+
+    .lite {
+      font-weight: 100;
+      margin-left: 8px;
+      font-size: 0.8rem;
     }
 
 </style>
